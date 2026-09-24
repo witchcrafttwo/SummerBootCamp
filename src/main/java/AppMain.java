5c769e01bb4faf98851fafb35b6cadf78ff87a9a
witchcrafttwo <bsi25009@uhe.ac.jp>
2026-09-24 23:41:41 +0900

    まだアプリとしては完成してないなんて言えない...

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -3,7 +3,7 @@ import view.RankingView;
 public class AppMain {
     public static void main(String[] args) {
         RankingView rankingView = new RankingView();
-        rankingView.rankview();
+        rankingView.rankview();//rankingを表示するだけ　orderbyしてない
 
 
 }
